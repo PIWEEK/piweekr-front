6b9eacd3155895a1107ab6e17ab3d2fcb07a604b
Jose Manuel Rosa <josemanuel.rosa@kaleidos.net>
2016-07-12 17:33:46 +0200

    Add avatar in cards

--- src/app/components/owner/owner.component.ts
+++ src/app/components/owner/owner.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 
+import { AvatarEditorComponent } from "../avatar-editor/avatar-editor.component";
+
+
 const BASE_DIR = './app/components/owner';
 
 @Component({
     selector: 'owner',
     templateUrl: `${BASE_DIR}/owner.component.html`,
-    styleUrls: [`${BASE_DIR}/owner.component.css`]
+    styleUrls: [`${BASE_DIR}/owner.component.css`],
+    directives: [AvatarEditorComponent]
 })
 
 export class OwnerComponent {
