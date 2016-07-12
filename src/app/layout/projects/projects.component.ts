a5e9a4733950cd6916e1077f287748597e2b89b6
Xavier Julián <xavier.julian@kaleidos.net>
2016-07-12 19:15:18 +0200

    Projects component

---
+++ src/app/layout/projects/projects.component.ts
@@ -0,0 +1,71 @@
+import {Component, Input} from "@angular/core";
+import { ApiService } from "../../services/api.service";
+import {VerticalListComponent} from "../../components/vertical-list/vertical-list.component";
+import {CardContainerComponent} from "../../components/card-container/card-container.component";
+
+@Component({
+    selector: "projects-layout",
+    templateUrl: "./app/layout/projects/projects.component.html",
+    styleUrls: ["./app/layout/projects/projects.component.css"],
+    directives: [VerticalListComponent, CardContainerComponent]
+})
+
+export class ProjectsComponent {
+    @Input() selectedItem: any;
+
+    type: string = 'Projects';
+    items = [];
+    ownerMenu: Array<Object>;
+
+    constructor(private _api: ApiService) {
+        this._api.projects.list().subscribe( projects =>
+            {
+                this.items = projects;
+            },
+            err => console.log(err)
+        );
+
+        this.ownerMenu = [
+            {
+                name: 'Borrar',
+                action: this.deleteAction
+            },
+            {
+                name: 'Editar',
+                action: this.editAction
+            },
+            {
+                name: 'Invitar',
+                action: this.inviteAction
+            },
+            {
+                name: 'Promocionar',
+                action: this.promoteAction
+            }
+        ];
+    }
+
+    handleSelected(item: string) {
+        this.selectedItem = item;
+    };
+
+    private deleteAction(id: string) {
+        console.log('deleteAction');
+        console.log('id', id);
+    }
+
+    private editAction(id: string) {
+        console.log('editAction');
+        console.log('id', id);
+    }
+
+    private inviteAction(id: string) {
+        console.log('inviteAction');
+        console.log('id', id);
+    }
+
+    private promoteAction(id: string) {
+        console.log('promoteAction');
+        console.log('id', id);
+    }
+}
--- src/app/router/app.routes.ts
+++ src/app/router/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from "../layout/login/login.component";
 import { IdeasComponent } from "../layout/ideas/ideas.component";
 import { MarketplaceComponent } from "../layout/marketplace/marketplace.component";
 import { ProfileComponent } from "../layout/profile/profile.component";
+import { ProjectsComponent } from "../layout/projects/projects.component";
 
 export const routes: RouterConfig = [
     {path: '',            component: HomeComponent},
@@ -11,6 +12,7 @@ export const routes: RouterConfig = [
     {path: 'ideas',       component: IdeasComponent},
     {path: 'marketplace', component: MarketplaceComponent},
     {path: 'profile',     component: ProfileComponent},
+    {path: 'projects',    component: ProjectsComponent},
 ];
 
 export const APP_ROUTER_PROVIDERS = [
