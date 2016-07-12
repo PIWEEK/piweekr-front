2d5996c8e7dc691699e377f0cc7c3d693ba88fe4
Xavier Julián <xavier.julian@kaleidos.net>
2016-07-12 15:04:08 +0200

    Add loader in components

---
+++ src/app/components/counterback/counterback.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "counterback",
+    templateUrl: "./app/components/counterback/counterback.component.html",
+    styleUrls: ["./app/components/counterback/counterback.component.css"]
+})
+
+export class CounterBackComponent {
+}
--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from "@angular/core";
 import {NavbarComponent} from "../../components/navbar/navbar.component";
 import {AboutComponent} from "../../components/about/about.component";
+import {CounterBackComponent} from "../../components/counterback/counterback.component";
 
 @Component({
     selector: "home-layout",
     templateUrl: "./app/layout/home/home.component.html",
     styleUrls: ["./app/layout/home/home.component.css"],
-    directives: [NavbarComponent, AboutComponent]
+    directives: [NavbarComponent, AboutComponent, CounterBackComponent]
 })
 
 export class HomeComponent {
