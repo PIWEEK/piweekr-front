7ce72d7ac66a591af10366c496f4899dd49ae057
Jose Manuel Rosa <josemanuel.rosa@kaleidos.net>
2016-07-15 11:48:58 +0200

    Filter projects

--- src/app/layout/projects/projects.component.ts
+++ src/app/layout/projects/projects.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input} from "@angular/core";
 import { ApiService } from "../../services/api.service";
 import { PublishService } from '../../services/publish.service';
-import {VerticalListComponent} from "../../components/vertical-list/vertical-list.component";
-import {CardContainerComponent} from "../../components/card-container/card-container.component";
+import { VerticalListComponent } from "../../components/vertical-list/vertical-list.component";
+import { CardContainerComponent } from "../../components/card-container/card-container.component";
 
 @Component({
     selector: "projects-layout",
@@ -17,9 +17,19 @@ export class ProjectsComponent {
 
     type: string = 'Projects';
     items = [];
+    itemsOriginal = [];
     ownerMenu: Array<Object>;
 
-    constructor(private _api: ApiService) {
+    constructor(
+        private _api: ApiService,
+        public publishService: PublishService
+    ) {
+        this.publishService.publish$
+            .filter(p => p.type === "filter")
+            .subscribe(p => {
+                this.filtersChange(p.item);
+            });
+
         this.ownerMenu = [
             {
                 name: 'Borrar',
@@ -43,10 +53,46 @@ export class ProjectsComponent {
     ngOnInit() {
         this._api.projects.list().subscribe(
             projects => {
-                this.items = projects;
+                this.itemsOriginal = projects;
+                // Fatest way to clone array
+                this.items = this.itemsOriginal.slice(0);
             },
             err => console.log(err)
         );
+    }
+
+    filtersChange(newFilters) {
+        // Hack for cast "true" to true, and "false" to false
+        const IS_PUBLIC = JSON.parse(newFilters.isPublic);
+        const IS_FORKED = JSON.parse(newFilters.isForked);
+        const TITLE = newFilters.title.toLowerCase();
+
+        // Fatest way to clone array
+        this.items = this.itemsOriginal.slice(0);
+
+        if (newFilters.title) {
+            let LOWER_CASE = undefined;
+            this.items = this.items.filter(
+                // Hack for cast "true" to true, and "false" to false
+                it => {
+                    LOWER_CASE = it.title.toLowerCase();
+                    return LOWER_CASE.includes(TITLE);
+                }
+            );
+        }
+
+        if (IS_PUBLIC !== null) {
+            this.items = this.items.filter(
+                // Hack for cast "true" to true, and "false" to false
+                it => it.isPublic === IS_PUBLIC
+            );
+        }
+
+        if (IS_FORKED !== null) {
+            this.items = this.items.filter(
+                it => !!it.forkedFrom === IS_FORKED
+            );
+        }
     }
 
     handleSelected(item: string) {
